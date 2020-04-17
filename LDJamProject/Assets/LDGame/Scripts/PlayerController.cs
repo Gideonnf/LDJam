854d5a6d1f3ad29096ffac6acaaf6c3e3f070051
gideon <gideonnfrancis999@gmail.com>
2020-04-18 02:05:05 +0800

    Testing something

---
+++ LDJamProject/Assets/LDGame/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    PlayerMovement m_PlayerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlayerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Interaction
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+
+    /// <summary>
+    /// For any interaction with E Key
+    /// </summary>
+    public void Interact()
+    {
+
+    }
+
+    /// <summary>
+    /// Flipping the sprite since its 2D
+    /// </summary>
+    public void Flip()
+    {
+
+    }
+
+}
---
+++ LDJamProject/Assets/LDGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody PlayerRB;
+
+    Vector3 movement;
+
+    [SerializeField] float MovementSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+       
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerRB.MovePosition(PlayerRB.position + movement * MovementSpeed * Time.fixedDeltaTime);
+    }
+
+    #region Movement Functions
+
+    public void Jump()
+    {
+        
+    }
+
+    public void MoveUp()
+    {
+
+    }
+
+    public void MoveLeft()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+
+    }
+
+    public void MoveDown()
+    {
+
+    }
+
+    #endregion
+
+}
