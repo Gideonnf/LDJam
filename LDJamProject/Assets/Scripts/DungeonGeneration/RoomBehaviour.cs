5e603a4aa650ae8d6d4359e99f3f76e90fe3764c
Angie <173123N@mymail.nyp.edu.sg>
2020-04-20 00:57:42 +0800

    - Room can show enemies now, sort of

--- LDJamProject/Assets/Scripts/DungeonGeneration/RoomBehaviour.cs
+++ LDJamProject/Assets/Scripts/DungeonGeneration/RoomBehaviour.cs
@@ -114,6 +114,11 @@ public class RoomBehaviour : MonoBehaviour
 
             if (enemy != null)
             {
+                enemy.SetActive(true);
+                EnemyBase enemyBase = enemy.GetComponent<EnemyBase>();
+                if (enemyBase)
+                    enemyBase.Init();
+
                 //spawn at a random location
                 int randomLocationIndex = Random.Range(0, m_PossibleEnemySpawnPosition.childCount - 1);
                 enemy.transform.position = m_PossibleEnemySpawnPosition.GetChild(randomLocationIndex).position;
