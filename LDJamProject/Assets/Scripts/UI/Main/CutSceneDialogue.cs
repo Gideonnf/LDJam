4aa1eebec9dd1702fe6c11c948f30ff22c9eecf6
Angie <173123N@mymail.nyp.edu.sg>
2020-04-21 02:12:14 +0800

    - Pushed start scene

---
+++ LDJamProject/Assets/Scripts/UI/Main/CutSceneDialogue.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class CutSceneDialogue : MonoBehaviour
+{
+    [HideInInspector]
+    public Queue<string> m_Sentences;
+
+    [HideInInspector]
+    public bool m_Talking = false;
+
+    [Header("Dialogue UI")]
+    public TextMeshProUGUI m_NameText;
+    public TextMeshProUGUI m_DialogueText;
+    public GameObject m_TextBox;
+    public GameObject m_ArrowText; //only shown when the speech is done
+    public float m_TextSpeed = 0.0f;
+    private Animator m_DialogueAnimator;
+
+    [Header("Sound")]
+    public string m_LetterSound;
+
+    private bool m_IsCouroutineRunning = false;
+    private string m_CurrentSentence;
+
+    private void Start()
+    {
+        m_Sentences = new Queue<string>();
+
+        if (m_TextBox != null)
+            m_DialogueAnimator = m_TextBox.GetComponentInChildren<Animator>();
+    }
+
+    //when interacted, call once
+    public void StartDialogue(Dialogue dialogue)
+    {
+        //play animation to pop up dialogue
+        ShowSpeechUI(true);
+        Talk(dialogue); //show text
+    }
+
+    public void Talk(Dialogue dialogue)
+    {
+        if (m_NameText != null)
+            m_NameText.text = dialogue.m_Name;
+
+        m_Sentences.Clear();
+
+        foreach (string sentence in dialogue.m_Sentences)
+        {
+            m_Sentences.Enqueue(sentence);
+        }
+
+        m_Talking = true;
+        DisplaySpeech();
+    }
+
+    public void DisplaySpeech()
+    {
+        if (!m_IsCouroutineRunning)
+        {
+            DisplayNextSentence();
+        }
+        else
+        {
+            //if player wants to skip the animation, it will speed through and print out fully
+            m_IsCouroutineRunning = false;
+            StopAllCoroutines();
+            m_DialogueText.text = m_CurrentSentence;
+
+            if (m_ArrowText != null)
+                m_ArrowText.SetActive(true);
+        }
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (m_Sentences.Count == 0) //reach end of queue
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = m_Sentences.Dequeue();
+        m_CurrentSentence = sentence;
+        StartCoroutine(TypeSentence(sentence));
+
+        if (m_ArrowText != null)
+            m_ArrowText.SetActive(false);
+    }
+
+    void EndDialogue()
+    {
+        m_Talking = false;
+
+        //does animation
+        ShowSpeechUI(false);
+    }
+
+    void ShowSpeechUI(bool active)
+    {
+        //TODO:: SHOW PROPER UI
+
+        if (m_TextBox != null)
+        {
+            m_TextBox.SetActive(active);
+        }
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        m_IsCouroutineRunning = true;
+
+        m_DialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            m_DialogueText.text += letter;
+            SoundManager.Instance.Play(m_LetterSound);
+
+            yield return new WaitForSeconds(m_TextSpeed);
+        }
+
+        if (m_ArrowText != null)
+            m_ArrowText.SetActive(true);
+
+        m_IsCouroutineRunning = false; //when couroutine stops
+    }
+}
---
+++ LDJamProject/Assets/Scripts/UI/Main/CutSceneTransition.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneTransition : MonoBehaviour
+{
+    [Header("Dialogue")]
+    public Dialogue[] m_DialogueTexts;
+    public GameObject[] m_CutScenes;
+    public GameObject m_StoryText;
+    public Animator m_FadeOutInAnimator;
+    public CutSceneDialogue m_CutSceneDialogue;
+
+    int m_CurrentDialogue = 0;
+    bool m_AnimPlaying = false;
+
+    public void Clicked()
+    {
+        if (!m_CutSceneDialogue.m_Talking && !m_AnimPlaying)
+        {
+            ++m_CurrentDialogue;
+
+            //start fade in and out anim
+            m_FadeOutInAnimator.SetTrigger("StartFade");
+            m_AnimPlaying = true;
+        }
+        else
+        {
+            m_CutSceneDialogue.DisplaySpeech(); //fast forward the speech
+        }
+    }
+
+    public void FadeOutFinish()
+    {
+        //set prev inactive
+        if (m_CurrentDialogue >= 1)
+            m_CutScenes[m_CurrentDialogue - 1].SetActive(false);
+
+        //set current active
+        if (m_CurrentDialogue >= m_CutScenes.Length)
+        {
+            SceneManager.LoadScene("GameScene");
+            return;
+        }
+        else if (m_CurrentDialogue == m_CutScenes.Length - 1)
+        {
+            m_StoryText.SetActive(false);
+        }
+
+        m_CutScenes[m_CurrentDialogue].SetActive(true);
+        m_FadeOutInAnimator.SetTrigger("FadeIn");
+    }
+
+    public void FadeInFinish()
+    {
+        if (m_CurrentDialogue < m_CutScenes.Length - 1)
+            m_StoryText.SetActive(true);
+
+        m_CutSceneDialogue.StartDialogue(m_DialogueTexts[m_CurrentDialogue]);
+
+        m_AnimPlaying = false;
+    }
+}
