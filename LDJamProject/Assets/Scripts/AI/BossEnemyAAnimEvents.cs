253218a286df986b8a3f6d3c067a0fcfd9ccebdd
Alonzo <judgementbearmaurice@gmail.com>
2020-04-21 02:26:00 +0800

    Updated boss. Now it can take damage and die. Animation events work as well

---
+++ LDJamProject/Assets/Scripts/AI/BossEnemyAAnimEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemyAAnimEvents : MonoBehaviour
+{
+    BossEnemyA enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponentInParent<BossEnemyA>();
+    }
+
+    void Remove()
+    {
+        enemy.Remove();
+    }
+}
